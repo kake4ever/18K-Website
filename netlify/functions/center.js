96e26a94890203b71933f44b3566ff1920c673f4
Kenny Pham <Kenpi@iMac.local>
2026-02-25 11:43:37 -0800

    Set center ID

--- netlify/functions/center.js
+++ netlify/functions/center.js
@@ -1,12 +1,9 @@
 const { ok, cors } = require('./_zenoti');
 
-// Hardcoded since guest-type API keys can't list centers
-// Center ID will be discovered via services endpoint
 exports.handler = async (event) => {
   if (event.httpMethod === 'OPTIONS') return { statusCode: 204, headers: cors, body: '' };
-  
   return ok({
-    id: process.env.ZENOTI_CENTER_ID || 'pending',
+    id: 'eca2792d-2bbb-4789-be99-6a263c609925',
     name: '18K Nail Boutique',
     code: '18K Nail',
     address: '1323 Lincoln Blvd, Santa Monica, CA 90401',
